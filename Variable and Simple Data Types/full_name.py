ab6a84fd7846e2276b58a7ddcf741f9d6973b48f
Robert Stankiewicz <robert.stankiewicz@gmail.com>
2020-12-29 14:52:08 +0100

    basic exercises

---
+++ Variable and Simple Data Types/full_name.py
@@ -0,0 +1,20 @@
+first_name = "mick"
+last_name = "jenkins"
+full_name = f"{first_name} {last_name}"
+
+print(full_name)
+print(f"Hello, {full_name.title()}!")
+
+message = f"\n\tHello, {full_name.title()}!"
+print(message)
+
+# format example
+full_name = "{} {}".format(first_name, last_name);
+print(full_name.title())
+
+# whitespace
+first_name = "   mick   "
+print(f"{first_name}")
+print(f"{first_name.lstrip()}")
+print(f"{first_name.rstrip()}")
+print(f"{first_name.strip()}")
--- Variable and Simple Data Types/name.py
+++ Variable and Simple Data Types/name.py
@@ -1,4 +1,4 @@
-name = "robert stankiewicz"
+name = "mick jenkins"
 print(name.title())
 print(name.upper())
 print(name.lower())
