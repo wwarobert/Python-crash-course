62375462b110d607a1b0c890d0ff0f486b71bc72
Robert Stankiewicz <robert.stankiewicz@gmail.com>
2021-01-06 17:04:23 +0100

    Lists create/ modify/ add elements

--- Introducing Lists/bicycles.py
+++ Introducing Lists/bicycles.py
@@ -2,4 +2,25 @@ bicycles = ['trek', 'cannondale', 'batavus', 'gazelle', 'cortina', 'koga']
 
 print(bicycles)
 print(bicycles[0])
+print(bicycles[1])
+print(bicycles[2])
+print("\n")
+
+#last element from the list
+print(bicycles[-1])
+print(bicycles[-2])
+print(bicycles[-3])
+print("\n")
+
 print(bicycles[4].title())
+
+message = f"My favourite bicycle is {bicycles[3].title()}"
+print(message)
+
+print("\n")
+
+cars = ['renault 9', 'alfa romeo', 'renault scenic', 'audi a3', 'volkswagen', 'nissan']
+print(f"The very first car I had was {cars[0].title()}")
+print(f"The car I liked the most was {cars[3].title()} or maybe {cars[-2].title()}")
+print(f"The most fancy cat I ever had was {cars[1].title()}")
+print(f"Currently I drive {cars[-1].title()}")
---
+++ Introducing Lists/motorocycles.py
@@ -0,0 +1,21 @@
+motorocycles = ['honda', 'yamaha', 'suzuki']
+print(motorocycles)
+
+print("\nModify element in the list:")
+motorocycles[0] = 'ducati'
+print(motorocycles)
+
+print("\nAdd element to the list:")
+motorocycles.append('honda')
+print(motorocycles)
+
+print("\nCreate list by adding elements:")
+motorocycles = []
+motorocycles.append('honda')
+motorocycles.append('yamaha')
+motorocycles.append('suzuki')
+print(motorocycles)
+
+print("\nInsert new element:")
+motorocycles.insert(2, 'ducati')
+print(motorocycles)
