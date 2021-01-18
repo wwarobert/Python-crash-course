3bf00840825a98c4ff3c07f09fb8d20bdc8f16e9
Robert Stankiewicz <robert.stankiewicz@gmail.com>
2021-01-18 17:37:29 +0100

    Tuples

---
+++ Introducing Lists/buffet.py
@@ -0,0 +1,9 @@
+foods = ('sausage', 'cheese', 'chicken wings', 'eggs', 'chorizo')
+print("Menu:")
+for food in foods:
+    print(food.title())
+
+print("\nNew menu:")
+foods = ('salami', 'cheese', 'chicken wings', 'scrambled eggs', 'chorizo')
+for food in foods:
+    print(food.title())
---
+++ Introducing Lists/dimensions.py
@@ -0,0 +1,12 @@
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+
+print("\nOriginal dimension:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (500, 17)
+print("\nModified dimension:")
+for dimension in dimensions:
+    print(dimension)
